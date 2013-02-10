70c956fe90f064f844187258140b4e77bc00fbb8
Eduardo Kasper <ehkasper@gmail.com>
2013-02-10 10:16:17 -0200

    Metodos e atributos base adicionados à classe Eloquent

--- src/EscapeWork/LaravelHelpers/Eloquent.php
+++ src/EscapeWork/LaravelHelpers/Eloquent.php
@@ -4,7 +4,50 @@ use Illuminate\Database\Eloquent\Model as Model;
 
 class Eloquent extends Model
 {
+    # regras de validações
+    public static $validationRules = array();
 
-        
+    # mensagens das validações
+    public static $validationMessages = array();
+
+    # mensagens das validações
+    public static $messages;
+
+    /**
+     * Método de valicação
+     */
+    public static function validate( $fields )
+    {
+        $validation = Validator::make( $fields, static::$validationRules, static::$validationMessages );
+
+        if( $validation->fails() )
+        {
+            static::$messages = $validation->messages()->getMessages();
+            
+            return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * putting data in a Object and returning it
+     *
+     * @static
+     * @param  array  $data 
+     * @return Object
+     */
+    public static function getDataFromArray(array $data)
+    {
+        $object = new get_called_class();
+
+        foreach( $data as $attribute => $value )
+        {
+            $object->$attribute = $value;
+        }
+
+        return $object;
+    }
 
 }
