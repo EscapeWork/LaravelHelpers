7d0d347c2b20c2796447c5f64710eb62553180db
Luis Dalmolin <luis.nh@gmail.com>
2013-02-19 09:35:23 -0300

    Adicionada a funcao formatSlug

--- readme.md
+++ readme.md
@@ -1,5 +1,52 @@
-LaravelHelpers
-==============
+# LaravelHelpers
 
-# Configuração
-Altere o Alias da Eloquent para `EscapeWork\LaravelHelpers\Eloquent`
+### Configuração
+
+- Altere o Alias da Eloquent para `EscapeWork\LaravelHelpers\Eloquent`;
+
+### Validação
+
+Apenas defina as regras de validação do seu model: 
+
+```php
+class User extends Eloquent
+{
+
+    public static $validationRules = array(
+        'title' => array('required')
+    );
+
+    public static $validationMessages = array(
+        'title.required' => 'O título é obrigatório'
+    );
+}
+```
+
+E depois, você já pode validar o seu Model.
+
+```php
+class UserTest extends TestCase
+{
+
+    public function testValidate()
+    {
+        $fields = [
+            'title' => 'Testing'
+        ];
+
+        $this->assertTrue( User::validate( $fields ) );
+    }
+}
+
+### Slug
+
+Para criar um slug de um título ou algum outro atributo, utilize a função `formatSlug()`;
+
+```php
+$fields = [
+    'title' => Input::get('title')
+    'slug'  => Brand::formatSlug( Input::get('title') )
+];
+
+Brand::create( $fields );
+```
--- src/EscapeWork/LaravelHelpers/Eloquent.php
+++ src/EscapeWork/LaravelHelpers/Eloquent.php
@@ -71,4 +71,42 @@ class Eloquent extends Model
 
         return $html;
     }
+
+    /**
+     * Formatando o slug
+     */
+    public static function formatSlug( $var, $removerPonto = true, $enc = 'UTF-8' )
+    {
+        $acentos = array(
+            'a' => '/&Agrave;|&Aacute;|&Acirc;|&Atilde;|&Auml;|&Aring;/',
+            'a' => '/&agrave;|&aacute;|&acirc;|&atilde;|&auml;|&aring;/',
+            'c' => '/&Ccedil;/',
+            'c' => '/&ccedil;/',
+            'e' => '/&Egrave;|&Eacute;|&Ecirc;|&Euml;/',
+            'e' => '/&egrave;|&eacute;|&ecirc;|&euml;/',
+            'i' => '/&Igrave;|&Iacute;|&Icirc;|&Iuml;/',
+            'i' => '/&igrave;|&iacute;|&icirc;|&iuml;/',
+            'n' => '/&Ntilde;/',
+            'n' => '/&ntilde;/',
+            'o' => '/&Ograve;|&Oacute;|&Ocirc;|&Otilde;|&Ouml;/',
+            'o' => '/&ograve;|&oacute;|&ocirc;|&otilde;|&ouml;/',
+            'u' => '/&Ugrave;|&Uacute;|&Ucirc;|&Uuml;/',
+            'u' => '/&ugrave;|&uacute;|&ucirc;|&uuml;/',
+            'y' => '/&Yacute;|&Yuml;/',
+            'y' => '/&yacute;|&yuml;/'
+        );
+
+        $especiais = array('/', '\\', '|', '*', ':', '[', ']', '{', '}', "'", '"', ',', '%', '@', '&', '(', ')', '¬', '#', '!', '?', 'ª', 'º', '¨', '°', '.');
+        
+        $var = str_replace($especiais, '', $var);
+        $var = preg_replace($acentos, array_keys($acentos), htmlentities($var, ENT_NOQUOTES, $enc));
+        $var = trim( $var );
+        $var = str_replace(' ', '-', $var);
+        $var = str_replace('---', '-', str_replace(' ', '-', $var));
+        $var = str_replace('_', '-', str_replace('--', '-', $var));
+        $var = mb_strtolower($var, $enc);
+        $var = preg_replace($acentos, array_keys($acentos), $var);
+
+        return $var;
+    }
 }
